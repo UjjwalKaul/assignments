288c1a0e0454e7ef1132ad00e7bb8545d9f9b217
Ujjawal Kaul <ujjwal171@outlook.com>
2024-02-05 02:22:02 +0530

    Solved anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,24 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  n1 = str1.length;
+  n2 = str2.length;
+
+  if (n1 != n2) {
+    return false;
+  }
+  arr1 = str1.split("").sort();
+  arr2 = str2.split("").sort();
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] != arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
